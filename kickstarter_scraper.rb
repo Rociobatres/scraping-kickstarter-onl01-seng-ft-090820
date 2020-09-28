a5eb62e8b8c686fc0de69d6acaaaea736bc743de
Rocio Batres <jonasgirl21@gmail.com>
2020-09-28 20:04:39 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,6 @@
 # require libraries/modules 
-require nokogiri 
+require 'nokogiri' 
+require 'pry'
 
  :projects => {
   "My Great Project"  => {
@@ -25,4 +26,7 @@ end
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
+# location: project.css("ul.project-meta span.location-name").text
+# percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+
 
