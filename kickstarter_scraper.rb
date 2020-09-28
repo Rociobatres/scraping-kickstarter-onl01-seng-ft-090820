d2518fd32e70b3c0e41c02f7f016a30c98c2c73e
Rocio Batres <jonasgirl21@gmail.com>
2020-09-28 20:07:53 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,26 +2,17 @@
 require 'nokogiri' 
 require 'pry'
 
- :projects => {
-  "My Great Project"  => {
-    :image_link => "Image Link",
-    :description => "Description",
-    :location => "Location",
-    :percent_funded => "Percent Funded"
-  },
-  "Another Great Project" => {
-    :image_link => "Image Link",
-    :description => "Description",
-    :location => "Location",
-    :percent_funded => "Percent Funded"
-  }
-}
-
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-end
-
+  
+  projects = {}
+  
+  kickstarter.css("li.project.grid_4").each do |project|
+    title = project.css()
+  end
+projects 
+end 
 #projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
